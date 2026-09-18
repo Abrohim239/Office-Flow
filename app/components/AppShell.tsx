0b817f8a63a00a256b551a96eac1b60adde341dc
Abrohim239 <rabbanimeheraj03@gmail.com>
2026-09-18 23:55:01 +0600

    Update AppShell.tsx

--- app/components/AppShell.tsx
+++ app/components/AppShell.tsx
@@ -1,1 +1,29 @@
+"use client";
 
+import { usePathname } from "next/navigation";
+import Sidebar from "./Sidebar";
+
+export default function AppShell({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  const publicPage =
+    pathname === "/view" || pathname === "/login";
+
+  return (
+    <div className={publicPage ? "public-layout" : "app-layout"}>
+      {!publicPage && <Sidebar />}
+
+      <div
+        className={
+          publicPage ? "public-content" : "main-content"
+        }
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
